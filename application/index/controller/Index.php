69c41ce9c7192f40fce06fc800d47fb7da92a979
谭旭 <hyden@yunnan.local>
2018-05-06 12:47:38 +0800

    fix bug

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -3,6 +3,8 @@ namespace app\index\controller;
 use think\Session;
 use think\Db;
 use app\index\model\User;
+use app\index\model\Questions;
+use app\index\model\PracticeData;
 use think\Log;
 
 function json($str) {
@@ -22,7 +24,7 @@ class Index
 
         try {
             if ($data->mode == '登录') {
-                $u = $user->get([ 'user_name'=> $data->userName]);
+                $u = $user->get(['user_name'=> $data->userName]);
                 if (!$u) {
                     return json([ 'code' => 1, 'errMsg' => '用户不存在']);
                 }
@@ -32,7 +34,7 @@ class Index
                 }
     
                 Session::set('userId', $u->id);
-                return json([ 'code' => 0, 'message' => '登录成功']);
+                return json([ 'code' => 0, 'message' => '登录成功', 'userId' => $u->id]);
     
             } else {
                 $user->user_name = trim($data->userName);
@@ -40,7 +42,7 @@ class Index
                 $user->save();
     
                 Session::set('userId', $user->id);
-                return json([ 'code' => 0, 'message' => '注册']);
+                return json([ 'code' => 0, 'message' => '注册成功', 'userId' => $user->id]);
             }   
         } catch(Exception $e) {
             return json([ 'code' => 2, 'message' => $e->message]);
@@ -52,7 +54,34 @@ class Index
         shell_exec('rm ./code');
         shell_exec('rm ./code.c');
         shell_exec('rm ./code.o');
-    
+
+        Log::record($data);
+        $practiceDataModel = new PracticeData();
+
+        if ($data->userId !== -1) {
+            $query = ['user_id' => $data->userId, 'question_id' => $data->questionId];
+            try {
+                $cur = $practiceDataModel->where($query)->find();
+                if ($cur) {
+                    $practiceDataModel->where($query)->update([
+                        "do_count" => $cur->do_count + 1,
+                        "answer" => $data->code,
+                        "do_date" => date("Y-m-d H:i:s")
+                    ]);
+                } else {
+                    $practiceDataModel->user_id = $data->userId;
+                    $practiceDataModel->question_id = $data->questionId;
+                    $practiceDataModel->do_count = 1;
+                    $practiceDataModel->answer = $data->code;
+                    $practiceDataModel->do_date = date("Y-m-d H:i:s");
+                    
+                    $practiceDataModel->save();
+                }
+            } catch(Exception $e) {
+                return json([ 'code' => 1, 'errMsg' => '服务器端异常！']);
+            }
+        }
+
         try {
             $codeFile = fopen("code.c", "w");
             fwrite( $codeFile, $data->code);
@@ -60,12 +89,45 @@ class Index
             return json([ 'code' => 1, 'errMsg' => '服务器端异常！']);
         }
 
-        shell_exec('gcc -c code.c');
+        $result = shell_exec('gcc -c code.c');
+
         shell_exec('gcc code.o -o code');
         $result = shell_exec('./code');
         if ($result) {
             return json([ 'code' => 0, 'result' => $result]);
         }
 
+    }
+
+    // 获取所有题目
+    public function getQuestions() {
+        $question = new Questions();
+        
+        return json_encode($question->select());
+    }
+    
+    // 根据id获取题目
+    public function getQuestion($id) {
+        $question = new Questions();
+
+        return json_encode($question->where('id', $id)->find());
+    }
+
+    // 完成题目
+    public function setQuestionDone($userId, $questionId) {
+        $practiceDataModel = new PracticeData();
+
+        try {
+            $practiceDataModel->where([
+                "user_id" => $userId,
+                "question_id" => $questionId
+            ])->update([
+                "done" => 1
+            ]); 
+
+            return json([ 'code' => 0]);  
+        } catch (Exception $e) {
+            return json([ 'code' => 1, 'errMsg' => '服务器端异常！']); 
+        }
     }
 }
--- application/route.php
+++ application/route.php
@@ -1,16 +1,18 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
-// +----------------------------------------------------------------------
 
 use think\Route;
 
 // 用户登录、注册
 Route::post('user', 'index/Index/userLogin');
+
+// 编译运行程序
 Route::post('run', 'index/Index/runCode');
+
+// 获取题库 
+Route::get('get_questions', 'index/Index/getQuestions');
+
+// 根据题目id获取题目
+Route::get('get_question', 'index/Index/getQuestion');
+
+// 设置完成题目
+Route::get('set_question_done', 'index/Index/setQuestionDone');
